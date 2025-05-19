325501c65e113ddc7af158378a4eb5677435afe6
nightclub.gm <kevincenteno39@gmail.com>
2025-05-18 22:01:44 -0500

    vfvbf

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 const Stake = require("./StakeModel");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const OWNER = "nightclub.gm";
 const MEMO = "staking";
